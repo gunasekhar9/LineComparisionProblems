24eea891c1c4b4366172d5c86f2e2f209d931469
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-04 12:36:52 +0530

    [gunasekhar9] : LIne Comparision is Created

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Line Comparison Computation Program");
+            LineCalculating lineOne = new LineCalculating(9.0, 1.0, 5.0, 3.0);
+            double lengthOne = lineOne.LengthCalculate();
+            Console.WriteLine("The length of the first line is {0}", lengthOne);
+            LineCalculating lineTwo = new LineCalculating(1.0, 5.0, 3.0, 9.0);
+            double lengthTwo = lineTwo.LengthCalculate();
+            Console.WriteLine("The length of the second line is {0}", lengthTwo);
+        }
+    }
+}
+      
+    
+
